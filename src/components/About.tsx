be376638bf232fb63c90fea9f6a772cf7eba9430
bpatter5 <bpatter5@masonlive.gmu.edu>
2022-03-28 06:31:55 -0400

    adding about updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,31 +17,47 @@ class About extends React.Component<React.HTMLAttributes<HTMLDivElement>> {
                 </div>
                 <br />
                 <br />
-                <div className="columns">
-                    <div className="column is-one-half">
+                <div className="tile">
+                    <div className="tile is-parent">
                         <div className="container">
-                            <div className="tile is-ancestor">
-                                <div className="tile" style={{
-                                    backgroundColor: "#933A16"
-                                }}>
-                                    <p>
-                                        Gather your content
-                                    </p>
+                            <div className="columns is-centered">
+                                <div className="column is-one-third is-flex">
+                                    <article className="tile is-child notification is-flex" style={{
+                                        background: "#933A16"
+                                    }}>
+                                        <div className="container has-text-centered">
+                                            <p className="is-capitalized has-text-white is-size-6 has-text-weight-semibold">
+                                                Gather your content
+                                            </p>
+                                        </div>
+                                    </article>
                                 </div>
-                                <div className="tile">
-                                    <p>
-                                        Add it to the search bar
-                                    </p>
+                                <div className="column is-one-third is-flex">
+                                    <article className="tile is-child notification is-flex" style={{
+                                        background: "#933A16"
+                                    }}>
+                                        <div className="container has-text-centered">
+                                            <p className="is-capitalized has-text-white is-size-6 has-text-weight-semibold">
+                                            Add it to the search bar
+                                            </p>
+                                        </div>
+                                    </article>
                                 </div>
-                                <div className="tile">
-                                    <p>
-                                        Watch your results stream in
-                                    </p>
+                                <div className="column is-one-third is-flex">
+                                    <article className="tile is-child notification is-flex" style={{
+                                        background: "#933A16"
+                                    }}>
+                                        <div className="container has-text-centered">
+                                            <p className="is-capitalized has-text-white is-size-6 has-text-weight-semibold">
+                                            Watch your results stream in
+                                            </p>
+                                        </div>
+                                    </article>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>                
+                </div>
             </div>
         )
     }
