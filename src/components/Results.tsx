400ffcc396729424e82345e92b0f4e20e41514f3
bpatter5 <bpatter5@masonlive.gmu.edu>
2022-04-07 06:07:13 -0400

    Updating results to include victory charts

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,8 +1,25 @@
 import React, { FunctionComponent } from "react";
+import { VictoryBar, VictoryChart } from 'victory' ;
 
 type Content = {
     document: string
 }
+
+type Word = {
+   word: string,
+   word_count: number
+}
+
+type Words = {
+    words: Word[]
+}
+
+const data = [
+    {word: "disease", count: 2},
+    {word: "polio", count: 4},
+    {word: "inhaler", count: 7},
+    {word: "covid", count: 10}        
+];
 
 export const Results: FunctionComponent<Content> = ({ document }) => {
     
@@ -12,10 +29,22 @@ export const Results: FunctionComponent<Content> = ({ document }) => {
             <div className="container" style={{
                 fontFamily: "Source Code Pro"
             }}>
-                <div className="box">
-                    This is a box
-                </div>
+                 <VictoryChart
+                     domainPadding={{ x: 10}}
+                     padding={{ left: 90, top: 50, right: 10, bottom: 50}}
+                 >
+                    <VictoryBar horizontal
+                        style={{
+                            data: {fill: "#933A16"}
+                        }}
+                        data={data}
+                        x="word"
+                        y="count"
+                    />
+                </VictoryChart>
             </div>
+           
+            
         </div>
     )
 }
